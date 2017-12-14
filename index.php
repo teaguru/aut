87dd41f882089b0c7b2ac637d47e24f7397f0881
teaguru@bk.ru <123654-q>
2017-12-14 23:28:55 +0300

    boot

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 require "db.php";
-
-
+require "libs.php";
 ?>
+
 <?php if( isset($_SESSION['logged_user'])) : ?>
-Авторизован!<br>
+	<div class="container">
+Авторизован!<br> <div>
+<a href ="/add.php">Добавить</a><br>
     Привет, <strong> <?php echo $_SESSION['logged_user']->login ; ?> </strong>
     <br>
     <a href ="/logout.php">Выйти!</a><br>
@@ -14,3 +16,8 @@ require "db.php";
 <a href ="/signup.php">Регистрация</a><br>
     Вы не авторизованы!
 <?php endif; ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+
+</body>
